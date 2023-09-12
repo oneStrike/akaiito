b5b85bdcc8ed4bdd752913b2b4130faf2f11d3c3
oneStrike <759994689@qq.com>
2023-09-12 23:20:28 +0800

    perf: 调整Dockerfile

--- packages/admin/src/components/basic/BasicTree.vue
+++ packages/admin/src/components/basic/BasicTree.vue
@@ -1,97 +1,96 @@
 <script setup lang="ts">
-import type { TreeProps } from "naive-ui";
-import { utils } from "@/utils/index";
+import type { TreeProps } from 'naive-ui'
+import { utils } from '@/utils/index'
 
 export interface BasicTreeProps {
-	accordion?: TreeProps["accordion"];
-	treeData: TreeProps["data"];
-	defaultSelectedKeys?: TreeProps["defaultSelectedKeys"];
-	keyField?: string;
-	labelField?: string;
-	search?: boolean;
-	searchPlaceholder?: string;
-	searchValue?: string;
-	showIrrelevantNodes?: boolean;
-	renderSuffix?: TreeProps["renderSuffix"];
-	fillAll?: boolean;//填充 全部  选项
+  accordion?: TreeProps['accordion']
+  treeData: TreeProps['data']
+  defaultSelectedKeys?: TreeProps['defaultSelectedKeys']
+  keyField?: string
+  labelField?: string
+  search?: boolean
+  searchPlaceholder?: string
+  searchValue?: string
+  showIrrelevantNodes?: boolean
+  renderSuffix?: TreeProps['renderSuffix']
+  fillAll?: boolean //填充 全部  选项
 }
 
 const props = withDefaults(defineProps<BasicTreeProps>(), {
-	accordion: false,
-	keyField: "value",
-	labelField: "label",
-	defaultSelectedKeys: () => [],
-	search: true,
-	searchValue: "",
-	searchPlaceholder: "请输入搜索内容",
-	showIrrelevantNodes: false
-});
+  accordion: false,
+  keyField: 'value',
+  labelField: 'label',
+  defaultSelectedKeys: () => [],
+  search: true,
+  searchValue: '',
+  searchPlaceholder: '请输入搜索内容',
+  showIrrelevantNodes: false
+})
 
 const emits = defineEmits<{
-	(event: "update:searchValue", data: string | number): void
-	(event: "selectChange", data: (string | number)[]): void
-}>();
+  (event: 'update:searchValue', data: string | number): void
+  (event: 'selectChange', data: (string | number)[]): void
+}>()
 
 const innerTreeData = computed(() => {
-	if (props.fillAll && props.treeData) {
-		const closedValue = utils._.clone(props.treeData);
-		closedValue.unshift({
-			[props.labelField]: "全部",
-			[props.keyField]: "all",
-			fillAll: true
-		});
-		return closedValue;
-	}
-	return props.treeData;
-});
+  if (props.fillAll && props.treeData) {
+    const closedValue = utils._.clone(props.treeData)
+    closedValue.unshift({
+      [props.labelField]: '全部',
+      [props.keyField]: 'all',
+      fillAll: true
+    })
+    return closedValue
+  }
+  return props.treeData
+})
 
-const searchValue = ref("");
+const interSearchValue = ref('')
 
 watch(
-	() => props.searchValue,
-	(value) => {
-		searchValue.value = value;
-	},
-	{ immediate: true }
-);
+  () => props.searchValue,
+  (value) => {
+    interSearchValue.value = value
+  },
+  { immediate: true }
+)
 
-watch(searchValue, (value) => {
-	emits("update:searchValue", value);
-});
+watch(interSearchValue, (value) => {
+  emits('update:searchValue', value)
+})
 
 //选中项调整
 const selectedChange = (val: (string | number)[]) => {
-	emits("selectChange", val);
-};
-
+  emits('selectChange', val)
+}
 </script>
 
 <template>
-	<div>
-		<div class="w_100 main_between cross_center" v-if="search">
-			<n-input
-				v-model:value="searchValue"
-				:placeholder="searchPlaceholder"
-				size="small"
-			/>
-			<div class="ml_16 flex_center">
-				<slot name="search-after"></slot>
-			</div>
-		</div>
-		<n-tree
-			class="mt_16"
-			block-line
-			:pattern="searchValue"
-			:data="innerTreeData"
-			:default-selected-keys="defaultSelectedKeys!"
-			:key-field="keyField"
-			:label-field="labelField"
-			:show-irrelevant-nodes="showIrrelevantNodes"
-			selectable
-			:render-suffix="renderSuffix"
-			@update:selected-keys="selectedChange"
-		/>
-	</div>
+  <div>
+    <div class="w_100 main_between cross_center" v-if="search">
+      <n-input
+        v-model:value="interSearchValue"
+        :placeholder="searchPlaceholder"
+        size="small"
+      />
+      <div class="ml_16 flex_center">
+        <slot name="search-after"></slot>
+      </div>
+    </div>
+    <n-tree
+      class="mt_16"
+      block-line
+      :pattern="interSearchValue"
+      :data="innerTreeData"
+      :default-selected-keys="defaultSelectedKeys!"
+      :key-field="keyField"
+      :label-field="labelField"
+      :show-irrelevant-nodes="showIrrelevantNodes"
+      selectable
+      :render-suffix="renderSuffix"
+      @update:selected-keys="selectedChange"
+    />
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
--- packages/admin/src/hooks/useRequestList.ts
+++ packages/admin/src/hooks/useRequestList.ts
@@ -1,73 +1,77 @@
-import type {
-	ListParamsData,
-	UseListData
-} from "@/typings/hook/useRequestList";
-import { utils } from "@/utils";
-import type { Ref } from "vue";
+import type { ListParamsData, UseListData } from '@/typings/hook/useRequestList'
+import { utils } from '@/utils'
+import type { Ref } from 'vue'
 
 export const useListData: UseListData = (options) => {
-	const api = options.api;
-	const refreshHook = options.refreshHook;
+  const api = options.api
+  const refreshHook = options.refreshHook
 
-	const basicParams = {
-		pageIndex: 0,
-		pageSize: 15,
-		sort: "",
-		sortField: ""
-	};
+  const basicParams = {
+    pageIndex: 0,
+    pageSize: 15,
+    sort: '',
+    sortField: ''
+  }
 
-	const initialParams = utils._.cloneDeep(options.params);
+  const initialParams = utils._.cloneDeep(options.params)
 
-	const listParams = ref();
+  const listParams = ref()
 
-	const loading = ref(false);
-	const listData = ref<any[]>([]);
-	const total = ref<number>(0);
+  const loading = ref(false)
+  const listData = ref<any[]>([])
+  const total = ref<number>(0)
 
-	//请求接口
-	const runApi = async (reset = false) => {
-		loading.value = true;
-		listParams.value = getParams(options.params, reset);
-		const res = await api(listParams.value);
-		options.data.value = res.list;
-		total.value = res.total;
-		loading.value = false;
-	};
+  //请求接口
+  const runApi = async (reset = false) => {
+    loading.value = true
+    listParams.value = getParams(options.params, reset)
+    const res = await api(listParams.value)
+    options.data.value = res.list
+    total.value = res.total
+    loading.value = false
+  }
 
-	const getParams = (val?: ListParamsData | Ref<ListParamsData>, reset = false) => {
-		if (!val) return basicParams;
-		if (reset) return initialParams;
-		if (isRef(val)) val = val.value;
-		const dateParams = val.createdAt ? useFormatDate(val.createdAt) : {};
-		delete val.createdAt;
-		const params = { ...dateParams, ...(Object.assign(utils._.cloneDeep(basicParams), val)) };
-		if (params.sort) {
-			params.sort = params.sort === "ascend" ? "asc" : "desc";
-		} else {
-			params.sort = "asc";
-		}
-		return params;
-	};
+  const getParams = (
+    val?: ListParamsData | Ref<ListParamsData>,
+    reset = false
+  ) => {
+    if (!val) return basicParams
+    if (reset) return initialParams
+    if (isRef(val)) val = val.value
+    const dateParams = val.createdAt ? useFormatDate(val.createdAt) : {}
+    delete val.createdAt
+    const params = {
+      ...dateParams,
+      ...Object.assign(utils._.cloneDeep(basicParams), val)
+    }
+    if (params.sort) {
+      params.sort = params.sort === 'ascend' ? 'asc' : 'desc'
+    } else {
+      params.sort = 'asc'
+    }
+    return params
+  }
 
+  watch(
+    () => options.params,
+    () => {
+      runApi()
+    },
+    { deep: true, immediate: true }
+  )
 
-	watch(() => options.params, val => {
-		runApi();
-	}, { deep: true, immediate: true });
+  //重置刷新
+  const refresh = async () => {
+    refreshHook && refreshHook()
+    await runApi(true)
+  }
 
-
-	//重置刷新
-	const refresh = async () => {
-		refreshHook && refreshHook();
-		await runApi(true);
-	};
-
-
-	return {
-		runApi,
-		refresh,
-		listData,
-		loading,
-		total,
-		params: listParams
-	};
-};
+  return {
+    runApi,
+    refresh,
+    listData,
+    loading,
+    total,
+    params: listParams
+  }
+}
