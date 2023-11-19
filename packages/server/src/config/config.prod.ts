a428b096f4b169913c5312b9879188f81c630e05
oneStrike <759994689@qq.com>
2023-11-19 22:53:41 +0800

    feat: 调整gitlab配置

--- packages/server/src/config/config.prod.ts
+++ packages/server/src/config/config.prod.ts
@@ -2,6 +2,6 @@ import { MidwayConfig } from '@midwayjs/core'
 
 export default {
   prisma: {
-    url: 'mysql://root:259158@172.17.0.1:3306/foo'
+    url: 'mysql://akaiito:259158@172.17.0.1:3306/foo'
   }
 } as MidwayConfig
