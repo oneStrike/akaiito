9f5354e8a9141256753d374799dad1d904b3f046
oneStrike <759994689@qq.com>
2023-11-20 22:28:42 +0800

    feat: 调整gitlab配置

--- packages/utils/test/index.ts
+++ packages/utils/test/index.ts
@@ -4,17 +4,17 @@ import { expect } from 'chai'
 import { Example } from '../dist/index'
 
 describe('Example class', () => {
-	it('should create an instance using its constructor', () => {
-		const example: Example = new Example()
-		expect(example, 'example should exist').to.exist
-	})
-	it('should return whatever is passed to exampleMethod()', () => {
-		const example: Example = new Example()
-		const param = 'This is my param.'
-		const returnValue: string = example.exampleMethod(param)
-		expect(returnValue).to.equal(
-			param,
-			'returns the value passed as a parameter'
-		)
-	})
+  it('should create an instance using its constructor', () => {
+    const example: Example = new Example()
+    expect(example, 'example should exist').to.exist
+  })
+  it('should return whatever is passed to exampleMethod()', () => {
+    const example: Example = new Example()
+    const param = 'This is my param.'
+    const returnValue: string = example.exampleMethod(param)
+    expect(returnValue).to.equal(
+      param,
+      'returns the value passed as a parameter'
+    )
+  })
 })
