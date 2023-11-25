f8047229fa17f7d7608a37c91e835c5930b56a77
oneStrike <759994689@qq.com>
2023-11-25 16:31:32 +0800

    feat: 调整gitlab配置

--- packages/server/scripts/compressPackage.js
+++ packages/server/scripts/compressPackage.js
@@ -10,9 +10,9 @@ module.exports = () => {
   const target = path.join(__dirname, '../dist/') //目标文件夹
   const source = path.join(__dirname, '../')
 
-  fs.moveSync(source + 'src', target + 'src')
-  fs.moveSync(source + 'package.json', target + 'package.json')
-  fs.moveSync(source + 'bootstrap.js', target + 'bootstrap.js')
+  fs.copySync(source + 'src', target + 'src')
+  fs.copySync(source + 'package.json', target + 'package.json')
+  fs.copySync(source + 'bootstrap.js', target + 'bootstrap.js')
   const packageJson = require(path.join(__dirname, '../dist/package.json'))
 
   for (const dependencyKey in packageJson.dependencies) {
