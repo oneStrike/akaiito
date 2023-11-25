c1756a7d90ed8252deea59abb279f0f21a6efa24
oneStrike <759994689@qq.com>
2023-11-25 21:07:16 +0800

    feat: 调整gitlab配置

--- packages/server/scripts/auxiliaryPacking.js
+++ packages/server/scripts/auxiliaryPacking.js
@@ -10,10 +10,12 @@ module.exports = () => {
   const target = path.join(__dirname, '../dist/') //目标文件夹
   const source = path.join(__dirname, '../')
 
-  fs.copySync(source + 'src', target + 'src')
-  fs.copySync(source + 'prisma', target + 'prisma')
-  fs.copySync(source + 'package.json', target + 'package.json')
-  fs.copySync(source + 'bootstrap.js', target + 'bootstrap.js')
+  const depend = ['src', 'prisma', 'package.json', 'bootstrap.js', '.env']
+
+  depend.forEach((item) => {
+    fs.copySync(source + item, target + item)
+  })
+
   const packageJson = require(path.join(__dirname, '../dist/package.json'))
 
   for (const dependencyKey in packageJson.dependencies) {
