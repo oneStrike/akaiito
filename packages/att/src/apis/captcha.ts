78153cbde378bac09520eb95e4abcdacbaa2dfdd
oneStrike <759994689@qq.com>
2024-10-06 18:14:59 +0800

    feat: 调整att

--- packages/att/src/apis/captcha.ts
+++
@@ -1,9 +0,0 @@
-import { httpClient } from '@/utils/request'
-import type { GetCaptchaTypings } from './captcha.d'
-
-export const getCaptchaApi = (): Promise<GetCaptchaTypings['Response']> => {
-  return httpClient({
-    method: 'GET',
-    url: '/open/captcha/getCaptcha',
-  })
-}
