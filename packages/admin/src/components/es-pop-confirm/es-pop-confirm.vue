254acd495340cb8c239b29f9d941549739b8591c
lik <2885038210@qq.com>
2024-11-26 08:29:42 +0800

    feat: 解析服务

--- packages/admin/src/components/es-pop-confirm/es-pop-confirm.vue
+++ packages/admin/src/components/es-pop-confirm/es-pop-confirm.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import type { AsyncFn, IterateObject } from '@akaiito/types'
 import { PromptsEnum } from '@/enum/prompts'
 import { useMessage } from '@/hooks/useFeedback'
 
@@ -7,25 +6,21 @@ export interface EsPopConfirmProps<T = IterateObject> {
   request: AsyncFn
   row: T
   ids?: boolean
-  loading?: boolean
 }
 
 const props = withDefaults(defineProps<EsPopConfirmProps>(), {})
 const emits = defineEmits<{
-  (event: 'update:row'): void
-  (event: 'update:loading'): void
   (event: 'success'): void
   (event: 'error', error: any): void
 }>()
 
-const row = useVModel(props, 'row', emits)
-const loading = useVModel(props, 'loading', emits)
+const loading = ref(false)
 
 async function deleteRow() {
   try {
     loading.value = true
     const params = {
-      [props.ids ? 'ids' : 'id']: props.ids ? [row.value.id] : row.value.id,
+      [props.ids ? 'ids' : 'id']: props.ids ? [props.row.id] : props.row.id,
     }
     await props.request(params)
     loading.value = false
@@ -46,13 +41,12 @@ async function deleteRow() {
     confirm-button-type="danger"
     title="是否删除当前项？"
     trigger="click"
+    :hide-after="0"
     @confirm="deleteRow"
   >
     <template #reference>
       <slot>
-        <el-button type="danger" link>
-          删除
-        </el-button>
+        <el-button type="danger" link :loading="loading"> 删除</el-button>
       </slot>
     </template>
   </el-popconfirm>
