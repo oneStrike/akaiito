1699f09a2ac1eb166962e560d73de61df28948f2
oneStrike <759994689@qq.com>
2024-12-02 13:32:51 +0800

    feat: apis

--- packages/server/src/prisma/index.ts
+++ packages/server/src/prisma/index.ts
@@ -13,31 +13,6 @@ export class RegisterPrisma {
         { level: 'query', emit: 'event' },
         { level: 'error', emit: 'event' },
       ],
-    }).$extends({
-      result: {
-        $allModels: {
-          createdAt: {
-            // @ts-expect-error ignore
-            needs: { createdAt: true },
-            compute(model) {
-              return new Date(new Date(model.createdAt).getTime() + 8 * 60 * 60 * 1000)
-                .toISOString()
-                .replace('T', ' ')
-                .substring(0, 19)
-            },
-          },
-          updatedAt: {
-            // @ts-expect-error ignore
-            needs: { updatedAt: true },
-            compute(model) {
-              return new Date(new Date(model.updatedAt).getTime() + 8 * 60 * 60 * 1000)
-                .toISOString()
-                .replace('T', ' ')
-                .substring(0, 19)
-            },
-          },
-        },
-      },
     })
 
     container.registerObject('prismaClient', prisma)
