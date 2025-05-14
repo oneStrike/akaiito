9bd426ba57d84929a8e2f5ae019e12ec5c8aa97d
lik <2885038210@qq.com>
2025-05-14 09:19:51 +0800

    build(dependencies): 更新依赖并调整样式处理
    - 移除 reset-css 和 normalize.css
    - 更新 uni-app 相关依赖版本
    - 更新 vite 版本
    -移除 unocss 相关未使用的依赖
    - 调整 unocss 配置，使用 presetWind4

--- packages/admin/src/main.ts
+++ packages/admin/src/main.ts
@@ -1,11 +1,9 @@
+import { createApp } from 'vue'
 import { bootstrap } from '@/core/bootstrap'
 import router from '@/router'
 import { stores } from '@/stores'
-import { createApp } from 'vue'
 import App from './App.vue'
 
-import 'reset-css'
-import 'normalize.css'
 import 'element-plus/es/components/message/style/css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import 'nprogress/nprogress.css'
--- packages/admin/unocss.config.ts
+++ packages/admin/unocss.config.ts
@@ -1,17 +1,35 @@
 import presetRemToPx from '@unocss/preset-rem-to-px'
-import { presetWind3 } from '@unocss/preset-wind3'
 import transformerVariantGroup from '@unocss/transformer-variant-group'
-import { defineConfig, presetAttributify } from 'unocss'
+import { defineConfig, presetAttributify, presetWind4 } from 'unocss'
 
 export default defineConfig({
   content: {
     pipeline: {
-      exclude: ['node_modules', 'dist', '.git', '.husky', '.vscode', 'public', 'build', 'mock', './stats.html'],
-      include: [/\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/, 'src/**/*.{js,ts}'],
+      exclude: [
+        'node_modules',
+        'dist',
+        '.git',
+        '.husky',
+        '.vscode',
+        'public',
+        'build',
+        'mock',
+        './stats.html',
+      ],
+      include: [
+        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        'src/**/*.{js,ts}',
+      ],
     },
   },
   transformers: [transformerVariantGroup()],
-  presets: [presetWind3(), presetAttributify(), presetRemToPx()],
+  presets: [
+    presetWind4({
+      reset: true,
+    }),
+    presetAttributify(),
+    presetRemToPx(),
+  ],
   shortcuts: [
     ['main-page', 'bg-[var(--el-bg-color)] p-3 overflow-hidden box-border'],
     ['wh-full', 'w-full h-full'],
@@ -39,39 +57,33 @@ export default defineConfig({
   rules: [
     [
       /^text-(.*)$/,
+      // @ts-expect-error ignore
       ([, c], { theme }) => ({ color: theme.colors[c] }),
-      { autocomplete: 'text-<theme|error|success|info|warning|primary|regular|secondary|disabled|borderColor>' },
+      {
+        autocomplete:
+          'text-<theme|error|success|info|warning|primary|regular|secondary|disabled|borderColor>',
+      },
     ],
     [
       /^border-(.*)$/,
-      ([, c], { theme }: { theme: any }) => ({ [`border-${c}`]: `1px solid ${theme.colors.borderColor}` }),
+      ([, c], { theme }: { theme: any }) => ({
+        [`border-${c}`]: `1px solid ${theme.colors.borderColor}`,
+      }),
       { autocomplete: 'border-<top|right|bottom|left>' },
     ],
   ],
-  theme:
-    {
-      colors: {
-        theme: 'var(--el-color-primary)',
-        error:
-          'var(--el-color-error)',
-        success:
-          'var(--el-color-success)',
-        info:
-          'var(--el-color-info)',
-        warning:
-          'var(--el-color-warning)',
-        primary:
-          'var(--el-text-color-primary)',
-        regular:
-          'var(--el-text-color-regular)',
-        secondary:
-          'var(--el-text-color-secondary)',
-        disabled:
-          'var(--el-text-color-disabled)',
-        borderColor:
-          'var(--el-border-color-light)',
-      }
-      ,
-    }
-  ,
+  theme: {
+    colors: {
+      theme: 'var(--el-color-primary)',
+      error: 'var(--el-color-error)',
+      success: 'var(--el-color-success)',
+      info: 'var(--el-color-info)',
+      warning: 'var(--el-color-warning)',
+      primary: 'var(--el-text-color-primary)',
+      regular: 'var(--el-text-color-regular)',
+      secondary: 'var(--el-text-color-secondary)',
+      disabled: 'var(--el-text-color-disabled)',
+      borderColor: 'var(--el-border-color-light)',
+    },
+  },
 })
