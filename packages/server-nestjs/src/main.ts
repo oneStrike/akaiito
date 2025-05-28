9a4fe44212f0169d437ed062c52cb1808e9bd43b
lik <2885038210@qq.com>
2025-05-28 18:25:05 +0800

    feat(server-nestjs): 添加 Prisma 支持并优化开发流程
    - 在 .gitignore 中添加 /generated/prisma 目录- 修改 AppService 中的 getHello 方法返回值
    - 在 main.ts 中添加热模块替换支持
    - 更新 package.json 中的脚本，使用 webpack 进行开发
    - 添加 Prisma 相关的依赖和脚本
    - 引入 run-script-webpack-plugin 和其他 webpack相关依赖

--- packages/server-nestjs/src/main.ts
+++ packages/server-nestjs/src/main.ts
@@ -4,5 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  // @ts-expect-error ignore
+  if (module.hot) {
+    // @ts-expect-error ignore
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    module.hot.accept();
+    // @ts-expect-error ignore
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
