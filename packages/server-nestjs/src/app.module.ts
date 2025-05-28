b314bcb517659d34dd2dcd939c40f350a2ffb48c
lik <2885038210@qq.com>
2025-05-28 19:41:13 +0800

    refactor(server-nestjs):修正 PrismaService 文件名引用- 将 prosma.service.ts 更名为 prisma.service.ts - 更新 app.module.ts 和 app.service.ts 中的引用

--- packages/server-nestjs/src/app.module.ts
+++ packages/server-nestjs/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prosma.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
